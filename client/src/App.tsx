24c8117e660e33b89983c519887d8f2a732fb12d
alejandro.tidele <alejandro.tidele@glovoapp.com>
2023-04-27 11:46:58 +0200

    Add the client upload button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Deli-Detective AI 🕵️‍♂️</h1>
+        <section>
+          <div>Upload an image 🪄</div>
+          <input type="file" className="magical-input" />
+        </section>
       </header>
     </div>
   );
